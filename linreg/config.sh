e60fc4c1d3844f1199c7dd080cc6be8e2f2a725e
racel <raceldamechli@outlook.fr>
2023-02-19 00:32:06 +0200

    config.sh configures venv and installs modules

--- linreg/config.sh
+++ linreg/config.sh
@@ -3,4 +3,5 @@ PYTHON=$(cut <<< $PYTHON_PATHS -d " " -f 1)
 $PYTHON -m venv "$(pwd)"
 source ./bin/activate
 echo Virtual environnement has been configured
-../bin/pip install numpy
+./bin/pip install numpy
+echo numpy is installed
