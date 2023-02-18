cbb5a462fb44c86789ee68d5b9e71a10ab6e3493
racel <raceldamechli@outlook.fr>
2023-02-19 00:19:04 +0200

    added bash script to configure virtual environement for linreg

---
+++ linreg/configure_venv.sh
@@ -0,0 +1,5 @@
+PYTHON_PATHS="$(whereis python | sed 's/python: //')"
+PYTHON=$(cut <<< $PYTHON_PATHS -d " " -f 1)
+$PYTHON -m venv "$(pwd)"
+source ./bin/activate
+echo Virtual environnement has been configured
