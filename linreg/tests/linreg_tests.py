43c35e8ba79c91accafdc48cbfd44b8d95c6dbe8
racel <raceldamechli@outlook.fr>
2023-03-06 13:54:33 +0200

    improved weights calculations

--- linreg/tests/linreg_tests.py
+++ linreg/tests/linreg_tests.py
@@ -1,9 +1,15 @@
 import unittest
+import numpy.testing as nptest
+import linreg.weights as weights
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+    @staticmethod
+    def test_weights_one_feature():
+        weights_vect = weights.get_weights([[5.6, 6.5, 6.8, 6.9, 7.0, 7.4, 8.0, 8.3, 8.7, 9.0]],
+                                           [5.0, 7.1, 8.4, 7.3, 7.8, 8.1, 7.4, 8.9, 9.0, 10.0])
+
+        nptest.assert_array_almost_equal(weights_vect, [[-0.342, 1.11]])  # add assertion here
 
 
 if __name__ == '__main__':
